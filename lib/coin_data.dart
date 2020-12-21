57c7e4b5a98008e87a1d21e05aec39cf53b58b42
louis again <louis2again@hotmail.com>
2020-12-21 12:54:45 +0000

    silver challenge done albeit cheated as ben so long but impressed still remember where code is

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -33,13 +33,13 @@ const List<String> cryptoList = [
 ];
 
 const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
-const apiKey = 'YOUR-API-KEY-HERE';
+const apiKey = '2A30DAB3-F9A1-4064-9C83-A8FFF34637C3';
 
 class CoinData {
   //TODO 3: Update getCoinData to take the selectedCurrency as an input.
-  Future getCoinData() async {
+  Future getCoinData(String selectedCurrency) async {
     //TODO 4: Update the URL to use the selectedCurrency input.
-    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    String requestURL = '$coinAPIURL/BTC$selectedCurrency?apikey=$apiKey';
     http.Response response = await http.get(requestURL);
     if (response.statusCode == 200) {
       var decodedData = jsonDecode(response.body);
